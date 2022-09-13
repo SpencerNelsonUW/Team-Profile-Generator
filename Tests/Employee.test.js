21102a2d4c83fdd1546e4e85d7b7f8b492d62a65
SpencerNelsonUW <spencernelsonn@gmail.com>
2022-09-12 17:53:23 -0700

    restructured the prompts and got them to ask questions and generate an HTML page

--- Tests/Employee.test.js
+++ Tests/Employee.test.js
@@ -1,1 +1,15 @@
-const Employee = require('../Lib/Employee')
+const Employee = require('../Lib/Employee');
+
+describe('Employee', () => {
+    it('can create an individual employee as an object', () => {
+        const employeeTest = new Employee();
+        expect(typeof(employeeTest)).toBe('object');
+    })
+
+    it('employee should have a name', () => {
+        const name = 'spencer';
+        const employeeTest = new Employee(name);
+        expect(employeeTest.name).toBe(name);
+    })
+
+})
